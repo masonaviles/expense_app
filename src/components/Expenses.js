555b1e33de08b6e472a866d6939c3a3c6f3e4102
Mason Aviles <mason_aviles@apple.com>
2023-02-08 16:16:52 -0800

    map through array to display component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,21 +4,14 @@ import ExpenseItem from "./ExpenseItem";
 function Expenses(props) {
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
+      {props.expenses.map((expense, key) => (
+        <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            key={key}
+        />
+      ))}
     </div>
   );
 }
